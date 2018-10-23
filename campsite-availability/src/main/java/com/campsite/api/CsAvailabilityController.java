21a89c49be9944f7ccee231c0eb7a2261a89d668
Pablo Ardiles <pablo.ardiles@synapse.com>
2018-10-22 22:47:28 -0300

    added swagger

--- campsite-availability/src/main/java/com/campsite/api/CsAvailabilityController.java
+++ campsite-availability/src/main/java/com/campsite/api/CsAvailabilityController.java
@@ -1,5 +1,6 @@
 package com.campsite.api;
 
+import com.campsite.error.CampsiteError;
 import com.campsite.model.Availability;
 import com.campsite.model.AvailabilityResponse;
 import com.campsite.model.CampsiteResponse;
@@ -7,9 +8,14 @@ import com.campsite.model.Reservation;
 import com.campsite.repository.AvailabilityRepository;
 import com.campsite.repository.ReservationRepository;
 import com.campsite.validator.DateValidator;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -17,6 +23,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@Api(value = "availability", description = "Endpoint for campsite availability")
 @RequestMapping("/api")
 public class CsAvailabilityController {
 
@@ -29,7 +36,11 @@ public class CsAvailabilityController {
     private AvailabilityRepository availabilityRepository;
 
 
-    @RequestMapping(value = "/availability", method = RequestMethod.GET)
+    @RequestMapping(value = "/availability", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Get campsite availability", response = AvailabilityResponse.class, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successful retrieval of availabilities", response = AvailabilityResponse.class),
+            @ApiResponse(code = 400, message = "Invalid parameters", response = CampsiteError.class)})
     public List<AvailabilityResponse> getAvailability(@RequestParam String date1, @RequestParam String date2) {
         DateValidator.validateDateRange(date1, date2);
         LocalDate d1 = LocalDate.parse(date1);

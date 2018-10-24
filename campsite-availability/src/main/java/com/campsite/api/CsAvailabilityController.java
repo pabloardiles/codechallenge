d6b34462ab76da60ef609ca6b045b738041291bf
Pablo Ardiles <pablo.ardiles@synapse.com>
2018-10-24 16:08:00 -0300

    organized imports

--- campsite-availability/src/main/java/com/campsite/api/CsAvailabilityController.java
+++ campsite-availability/src/main/java/com/campsite/api/CsAvailabilityController.java
@@ -10,9 +10,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
